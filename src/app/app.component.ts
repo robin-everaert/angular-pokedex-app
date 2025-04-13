333b6d6e6c3b18c2f97199e0f20d9ef34276ce68
Everaert Robin <kibs8@proton.me>
2025-04-14 00:19:21 +0200

    added directive - pipes - services
    component ready for merge into main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,42 @@
-import { Component, computed, signal } from '@angular/core';
-import { POKEMON_LIST } from './pokemon-list.fake';
+import { Component, computed, inject, signal } from '@angular/core';
 import { Pokemon } from './pokemon.model';
+import { PokemonBorderDirective } from './pokemon-border.directive';
+import { DatePipe } from '@angular/common';
+import { PokemonService } from './pokemon.service';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [PokemonBorderDirective, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
-    pokemonList = signal(POKEMON_LIST);
-   life = signal(21);
-   size(pokemon: Pokemon) {
-    if(pokemon.life <= 15) {
-        return 'Petit';
-    } 
-    if(pokemon.life >= 25) {
-        return 'Grand';
-    }
-    return 'Moyen';
-   }
+    readonly #pokemonService = inject(PokemonService);
+    readonly pokemonList = signal(this.#pokemonService.getPokemonList());
+    readonly searchTerm = signal('');
 
-   incrementLife(pokemon: Pokemon) {
-     pokemon.life = pokemon.life + 1;
-   }
-   
-   decrementLife(pokemon: Pokemon) {
-        pokemon.life = pokemon.life - 1;
-   }
+    readonly pokemonListFiltered = computed(() => {
+        const pokemonList = this.pokemonList();
+        const searchTerm = this.searchTerm().toLowerCase();
+        return pokemonList.filter(pokemon => pokemon.name.toLocaleLowerCase().includes(searchTerm.trim().toLocaleLowerCase()))
+    });
+
+    size(pokemon: Pokemon) {
+        if(pokemon.life <= 15) {
+            return 'Petit';
+        } 
+        if(pokemon.life >= 25) {
+            return 'Grand';
+        }
+        return 'Moyen';
+    }
+
+    incrementLife(pokemon: Pokemon) {
+        pokemon.life = pokemon.life + 1;
+    }
+    
+    decrementLife(pokemon: Pokemon) {
+            pokemon.life = pokemon.life - 1;
+    }
 } 
